889626cdcee17bd25ca35040dd38e331777362a5
Umair Asim <umair.asim.skipq@gmail.com>
2023-02-28 17:17:54 +0500

    github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
   base: "/note-app/",
 });
